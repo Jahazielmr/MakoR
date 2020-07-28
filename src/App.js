14fe811fa01d9a4b24a92ab46d4f0d7b8160deb2
Jahazielmr <Jahazielmr97@gmail.com>
2020-07-28 12:40:06 -0600

    Landing page and search component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Search from './Components/Search';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  state = {
+    textData1 : ''
+  }
+
+  APIconsult = () => {
+    const url = `https://pixabay.com/api/?key=17658002-78004488455a0b123ed9c9f52&q=${this.state.textData1}`;    
+    console.log(url);   
+
+    /*Usamos fetch para interpretar los archivos jason generados*/
+    fetch(url)
+    .then(returnR => returnR.json())
+    .then(resultado => console.log(resultado))
+  }
+
+  searchData = (textData) => {
+    
+    this.setState({
+      textData1: textData
+    }, () => {
+      /* Hacer la busqueda de imagenes con el state actual*/
+      this.APIconsult();
+    })
+
+
+ 
+  }
+
+
+  render() {
+    return (
+      <div className="app Container">
+
+        <div id="jumb" className="jumbotron">
+
+          <h1 className="lead text-center" id="MainTittle"> Mako Emerald </h1>
+
+        </div>
+
+        <Search
+
+          searchData={this.searchData}
+
+        />
+
+        {this.state.textData1}
+
+      </div>
+
+    );
+  }
 }
 
+
+
+
 export default App;
+
